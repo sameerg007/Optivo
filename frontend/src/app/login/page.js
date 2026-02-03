5e55fe35776b2c8e04bd0c243d10033475bfab4e
SAMEER GUPTA <sameergupta@SAMEERs-MacBook-Pro.local>
2026-02-03 11:47:45 +0530

    Added the default exports to fix the routing issue

---
+++ frontend/src/app/login/page.js
@@ -0,0 +1,4 @@
+'use client';
+
+// Re-export the Login component from pages/login
+export { default } from '../../../pages/login/page';
---
+++ frontend/src/app/signup/page.js
@@ -0,0 +1,4 @@
+'use client';
+
+// Re-export the SignUp component from pages/signup
+export { default } from '../../../pages/signup/page';
